af70fce24fa997f848b4839bd523534825887cb7
arnoldsouth <arnoldsouth@gmail.com>
2023-04-07 02:53:26 -0500

    add helper function to app, update css

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import './App.css'
+
+/* Helper Functions */
+function padWithZeros(number, width) {
+  const numberString = String(number)
+  return numberString.length >= width
+    ? numberString
+    : new Array(width - numberString.length + 1).join('0') + numberString
+}
 
 function App() {
   const [pokemon, setPokemon] = useState([])
   const [selectedPokemon, setSelectedPokemon] = useState(null)
   const [offset, setOffset] = useState(0)
-  const limit = 20
+  const limit = 9
 
   useEffect(() => {
     const fetchPokemon = async () => {
@@ -26,8 +35,11 @@ function App() {
   const handlePrevClick = () => setOffset(Math.max(0, offset - limit))
 
   return (
-    <div>
-      <h1>Pokemon</h1>
+    <div className="container">
+      <div className="logo">
+        <div className="logo-small">pokemon</div>DATABASE
+      </div>
+
       <ul>
         {pokemon.map((p) => (
           <li key={p.name} onClick={() => handlePokemonClick(p.name)}>
@@ -35,67 +47,125 @@ function App() {
           </li>
         ))}
       </ul>
-      <button onClick={handlePrevClick}>Prev</button>
-      <button onClick={handleNextClick}>Next</button>
-      {selectedPokemon && (
-        <div>
-          <div># {selectedPokemon.id}</div>
-          <div>{selectedPokemon.name}</div>
-          <img
-            src={selectedPokemon.sprites.front_default}
-            alt={selectedPokemon.name}
-          />
 
-          <div>
-            Type:{' '}
-            {selectedPokemon.types.length > 1
-              ? `${selectedPokemon.types[0].type.name} and ${selectedPokemon.types[1].type.name}`
-              : `${selectedPokemon.types[0].type.name}`}
-          </div>
+      <div className="prev-next-button">
+        <button onClick={handlePrevClick}>Previous</button>
+        <button onClick={handleNextClick}>Next</button>
+      </div>
 
-          <div>
-            Abilities:{' '}
-            {/* {selectedPokemon.abilities.map((ability) => (
+      <div className="card-container">
+        {selectedPokemon && (
+          <div className="pokemon-card">
+            <div>
+              <div className="pokemon-card-id">
+                # {padWithZeros(selectedPokemon.id, 4)}
+              </div>
+              <div className="pokemon-card-name">{selectedPokemon.name}</div>
+            </div>
+
+            <div className="pokemon-card-stats-detail-title">
+              {selectedPokemon.types.length > 1
+                ? `${selectedPokemon.types[0].type.name} and ${selectedPokemon.types[1].type.name}`
+                : `${selectedPokemon.types[0].type.name}`}
+            </div>
+
+            <img
+              src={selectedPokemon.sprites.front_default}
+              alt={selectedPokemon.name}
+            />
+            {/* <div className="pokemon-card-type">
+              {selectedPokemon.types.length > 1
+                ? `${selectedPokemon.types[0].type.name} and ${selectedPokemon.types[1].type.name}`
+                : `${selectedPokemon.types[0].type.name}`}
+            </div> */}
+            <div>
+              <div className="pokemon-card-stats-detail-title">Abilities </div>
+              {/* {selectedPokemon.abilities.map((ability) => (
               <span key={ability.ability.name}>
                 {ability.ability.name.split('-').join(' ')}
               </span>
             ))} */}
-            {selectedPokemon.abilities.length > 1
-              ? `${selectedPokemon.abilities[0].ability.name} and ${selectedPokemon.abilities[1].ability.name}`
-              : `${selectedPokemon.abilities[0].ability.name}`}
-          </div>
+              <div className="pokemon-card-stats-detail">
+                {selectedPokemon.abilities.length > 1
+                  ? `${selectedPokemon.abilities[0].ability.name} and ${selectedPokemon.abilities[1].ability.name}`
+                  : `${selectedPokemon.abilities[0].ability.name}`}
+              </div>
+            </div>
 
-          <div>
-            {selectedPokemon.stats[0].stat.name}:{' '}
-            {selectedPokemon.stats[0].base_stat}
-          </div>
-          <div>
-            {selectedPokemon.stats[1].stat.name}:{' '}
-            {selectedPokemon.stats[1].base_stat}
-          </div>
-          <div>
-            {selectedPokemon.stats[2].stat.name}:{' '}
-            {selectedPokemon.stats[2].base_stat}
-          </div>
-          <div>
-            {selectedPokemon.stats[3].stat.name}:{' '}
-            {selectedPokemon.stats[3].base_stat}
-          </div>
-          <div>
-            {selectedPokemon.stats[4].stat.name}:{' '}
-            {selectedPokemon.stats[4].base_stat}
-          </div>
-          <div>
-            {selectedPokemon.stats[5].stat.name}:{' '}
-            {selectedPokemon.stats[5].base_stat}
-          </div>
+            <div className="border-div"></div>
 
-          <div>Height: {(selectedPokemon.height * 0.328084).toFixed(1)} ft</div>
-          <div>
-            Weight: {(selectedPokemon.weight * 0.220462).toFixed(1)} lbs
+            <div className="pokemon-card-stats-detail-title pokemon-card-stats-header">
+              Base Stats
+            </div>
+            <div>
+              <div className="pokemon-card-stats-detail-title">
+                {selectedPokemon.stats[0].stat.name}{' '}
+              </div>
+              <div className="pokemon-card-stats-detail">
+                {selectedPokemon.stats[0].base_stat}
+              </div>
+            </div>
+            <div>
+              <div className="pokemon-card-stats-detail-title">
+                {selectedPokemon.stats[1].stat.name}{' '}
+              </div>
+              <div className="pokemon-card-stats-detail">
+                {selectedPokemon.stats[1].base_stat}
+              </div>
+            </div>
+            <div>
+              <div className="pokemon-card-stats-detail-title">
+                {selectedPokemon.stats[2].stat.name}{' '}
+              </div>
+              <div className="pokemon-card-stats-detail">
+                {selectedPokemon.stats[2].base_stat}
+              </div>
+            </div>
+            <div>
+              <div className="pokemon-card-stats-detail-title">
+                {selectedPokemon.stats[3].stat.name}{' '}
+              </div>
+              <div className="pokemon-card-stats-detail">
+                {selectedPokemon.stats[3].base_stat}
+              </div>
+            </div>
+            <div>
+              <div className="pokemon-card-stats-detail-title">
+                {selectedPokemon.stats[4].stat.name}{' '}
+              </div>
+              <div className="pokemon-card-stats-detail">
+                {selectedPokemon.stats[4].base_stat}
+              </div>
+            </div>
+            <div>
+              <div className="pokemon-card-stats-detail-title">
+                {selectedPokemon.stats[5].stat.name}{' '}
+              </div>
+              <div className="pokemon-card-stats-detail">
+                {selectedPokemon.stats[5].base_stat}
+              </div>
+            </div>
+            <div>
+              <div className="pokemon-card-stats-detail-title">
+                Height (ft){' '}
+              </div>
+
+              <div className="pokemon-card-stats-detail">
+                {(selectedPokemon.height * 0.328084).toFixed(1)}
+              </div>
+            </div>
+            <div>
+              <div className="pokemon-card-stats-detail-title">
+                Weight (lbs){' '}
+              </div>
+
+              <div className="pokemon-card-stats-detail">
+                {(selectedPokemon.weight * 0.220462).toFixed(1)}
+              </div>
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   )
 }
--- server/controllers/pokemonControllers.js
+++ server/controllers/pokemonControllers.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 const getPokemonList = async (req, res) => {
   try {
     const offset = req.query.offset || 0
-    const limit = req.query.limit || 20
+    const limit = req.query.limit || 9
     const response = await axios.get(
       `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
     )
