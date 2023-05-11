5573ff0e4085a8f27168d79a6ac8e4071e77ca8c
arnoldsouth <arnoldsouth@gmail.com>
2023-05-11 16:07:40 -0500

    add serverless function and update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -28,7 +28,7 @@ function App() {
             // )
 
             const response = await axios.get(
-                `https://pokemon-database-card.vercel.app/api/getPokemonList`
+                `https://pokemon-database-card.vercel.app/api/getPokemonList?offset=${offset}&limit=${limit}&search=${searchTerm}`
             )
             setPokemon(response.data.results)
         }
